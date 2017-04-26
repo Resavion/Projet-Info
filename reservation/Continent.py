b70190ea71c82124037f072f837e3a839031480b
resavion <resavion.ensg@gmail.com>
2017-04-26 10:22:21 +0200

    Déplacé fichiers

---
+++ reservation/Continent.py
@@ -0,0 +1,11 @@
+class Continent(object):
+    def __init__(self,id_continent,nom_continent):
+        """
+        Constructeur de la classe Continent
+        
+        :param id_continent: identifiant du continent
+        :param nom_continent: nom du continent
+        """
+        self._id_continent = id_continent
+        self.nom_continent = nom_continent
+

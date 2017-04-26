d95108a6603500ad5d9b8331935feab64487a8a4
resavion <resavion.ensg@gmail.com>
2017-04-26 12:07:57 +0200

    classe compagnie changée

--- reservation/Compagnie.py
+++ reservation/Compagnie.py
@@ -3,29 +3,23 @@ class Compagnie(object):
         """
         Constructeur de la classe compagnie
         
-        :param id_compagnie: 
-        :param nom_compagnie: 
-        :param alias: 
-        :param iata: 
-        :param icao: 
-        :param callsign: 
-        :param pays: 
-        :param actif: 
+        :param id_compagnie: identifiant de la compagnie
+        :param nom_compagnie: nom de la compagnie
+        :param alias: alias de la compagnie
+        :param iata: code iata de la compagnie
+        :param icao: code icao de la compagnie
+        :param callsign: callsigne de la compagnie
+        :param pays: pays d'origine de la compagnie
+        :param actif: si la compagnie est toujours active
         """
 
-        :param
-        id_compagnie:
-        :param
-        nom_compagnie:
-        :param
-        alias:
-        :param
-        iata:
-        :param
-        icao:
-        :param
-        callsign:
-        :param
-        pays:
-        :param
-        actif:
+        self._id_compagnie = id_compagnie
+        self._nom_compagnie = nom_compagnie
+        self._alias = alias
+        self._iata = iata
+        self._icao = icao
+        self._callsign = callsign
+        self._pays = pays
+        self._actif= actif
+
+    

be8d9a0b67488a2be555f7f065dd4c5218ec6860
resavion <resavion.ensg@gmail.com>
2017-04-26 12:02:50 +0200

    Classe compagnie

---
+++ reservation/Compagnie.py
@@ -0,0 +1,31 @@
+class Compagnie(object):
+    def __init__(self,  id_compagnie, nom_compagnie, alias, iata, icao, callsign, pays, actif):
+        """
+        Constructeur de la classe compagnie
+        
+        :param id_compagnie: 
+        :param nom_compagnie: 
+        :param alias: 
+        :param iata: 
+        :param icao: 
+        :param callsign: 
+        :param pays: 
+        :param actif: 
+        """
+
+        :param
+        id_compagnie:
+        :param
+        nom_compagnie:
+        :param
+        alias:
+        :param
+        iata:
+        :param
+        icao:
+        :param
+        callsign:
+        :param
+        pays:
+        :param
+        actif:

4a794256c072ae9481d332544f159a0c694e3c2c
Mathieu Margollé <mathieu.margolle@yandex.com>
2017-04-27 23:02:42 +0200

    Fonctions grands cercles Route

--- reservation/Route.py
+++ reservation/Route.py
@@ -1,21 +1,23 @@
+import math as ma
+import reservation.Aeroport
+
+
 class Route(object):
-    def __init__(self,  id_compagnie, aeroport_depart, aeroport_arrivee, geom, distance, codeshare):
+    def __init__(self,  id_compagnie, aeroport_depart, aeroport_arrivee, geom, codeshare):
         """
         Constructeur de la classe route
         
         :param id_compagnie: identifiant de la compagnie
         :param aeroport_depart: aeroport de depart de la route
         :param aeroport_arrivee: aeroport d'arrivee de la route
-        :param geom: coordonnées de l'aeroport de depart
-        :param distance: distance separant les deux aeroports
-        :param codeshare: booleen qui permet de savoir si un avion est partagé par plusieurs compagnies
+        :param geom: linestring entre les deux aeroports en WKT
+        :param codeshare: booleen qui permet de savoir si un avion est partage par plusieurs compagnies
         """
-
         self._id_compagnie = id_compagnie
         self._aeroport_depart = aeroport_depart
         self._aeroport_arrivee = aeroport_arrivee
         self._geom = geom
-        self._distance = distance
+        self._distance = self.distance_haversine(aeroport_depart, aeroport_arrivee)
         self._codeshare = codeshare
 
     @property
@@ -24,23 +26,58 @@ class Route(object):
 
     @property
     def aeroport_depart(self):
-        return self._id_compagnie
+        return self._aeroport_depart
 
     @property
     def aeroport_arrivee(self):
-        return self._id_compagnie
+        return self._aeroport_arrivee
 
     @property
     def geom(self):
-        return self._id_compagnie
+        return self._geom
 
     @property
     def distance(self):
-        return self._id_compagnie
+        return self._distance
 
     @property
     def codeshare(self):
-        return self._id_compagnie
+        return self._codeshare
+
+    @staticmethod
+    def distance_haversine(self, dep, arr, radius=6371000):
+        """ note that the default distance is in meters """
+        dlat = ma.radians(arr.latitude - dep.latitude)
+        dlon = ma.radians(arr.longitude - dep.longitude)
+        lat1 = ma.radians(dep.latitude)
+        lat2 = ma.radians(arr.latitude)
+        a = ma.sin(dlat / 2) * ma.sin(dlat / 2) + ma.sin(dlon / 2) * ma.sin(dlon / 2) * ma.cos(lat1) * ma.cos(lat2)
+        c = 2 * ma.atan2(ma.sqrt(a), ma.sqrt(1 - a))
+        return c * radius
+
+    @staticmethod
+    def bearing(self, dep, arr):
+        dlon = ma.radians(arr.longitude - dep.longitude)
+        lat1 = ma.radians(dep.latitude)
+        lat2 = ma.radians(arr.latitude)
+        y = ma.sin(dlon) * ma.cos(lat2)
+        x = ma.cos(lat1) * ma.sin(lat2) - ma.sin(lat1) * ma.cos(lat2) * ma.cos(dlon)
+        return ma.degrees(ma.atan2(y, x))
+
+    def waypoint(self, dep, arr, frac, radius=6371000):
+        delta = self._distance/radius
+        a = ma.sin((1. - frac)*delta)/ma.sin(delta)
+        b = ma.sin(frac*delta)/ma.sin(delta)
+        lat1 = ma.radians(dep.latitude)
+        lat2 = ma.radians(arr.latitude)
+        lon1 = ma.radians(dep.longitude)
+        lon2 = ma.radians(arr.longitude)
+        x = a * ma.cos(lat1) * ma.cos(lon1) + b * ma.cos(lat2) * ma.cos(lon2)
+        y = a * ma.cos(lat1) * ma.sin(lon1) + b * ma.cos(lat2) * ma.sin(lon2)
+        z = a * ma.sin(lat1) + b * ma.sin(lat2)
+        lat = ma.atan2(z, ma.sqrt(x**2 + y**2))
+        lon = ma.atan2(y, x)
+        return lat, lon
 
     def ajouter_horaire(self):
         """
@@ -66,8 +103,8 @@ class Route(object):
 
     def afficher_stats(self):
         """
-        Methode qui permet d'afficher les statistiques sur le nombre personnes ayant réellement empruntés le chemin par rapport
-        au nombre de places offertes
+        Methode qui permet d'afficher les statistiques sur le nombre de personnes ayant réellement
+        emprunté le chemin par rapport au nombre de places offertes
         :return: 
         """
         pass

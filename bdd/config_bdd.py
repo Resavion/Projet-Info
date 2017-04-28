6dea402496090562927a4e3177b62bb80c0e094f
Mathieu Margollé <mathieu.margolle@yandex.com>
2017-04-28 02:35:54 +0200

    Suite config BDD (ajout ConfigAvion)

--- bdd/config_bdd.py
+++ bdd/config_bdd.py
@@ -15,7 +15,7 @@ def creer_bdd(db_name):
 
     # Création des tables
     req = """create table EnumAvion (id integer primary key AUTOINCREMENT NOT NULL,
-                                    statut text)"""
+                                    etat text)"""
     executer_requete(cur, req)
     req = """create table EnumOption (id integer primary key AUTOINCREMENT NOT NULL,
                                     option text)"""
@@ -48,31 +48,40 @@ def creer_bdd(db_name):
                                      icao_code text,
                                      pays text)"""
     executer_requete(cur, req)
-    req = """create table TypeAvion (id integer primary key AUTOINCREMENT NOT NULL,
-                                    nom text,
-                                    nb_place_premiere integer,
-                                    nb_place_business integer,
-                                    nb_place_eco_plus integer,
-                                    nb_place_eco integer,
-                                    nb_total_place integer,
+    req = """create table TypeAvion (id text primary key,
+                                    iata_code text,
+                                    icao_code text,
                                     fuel_cap_L integer,
                                     distance_franchissable_km integer,
                                     vitesse_mach real,
                                     altitude_vol_m integer,
                                     distance_decollage_m integer)"""
     executer_requete(cur, req)
-    req = """create table Avion (id integer primary key AUTOINCREMENT NOT NULL,
+    req = """create table ConfigAvion (id text,
+                                    id_compagnie text,
+                                    primary key(nom, id_compagnie),
+                                    id_type_avion text,
+                                    nb_place_premiere integer,
+                                    nb_place_business integer,
+                                    nb_place_eco_plus integer,
+                                    nb_place_eco integer,
+                                    nb_total_place integer,
+                                    disposition text,
+                                    foreign key(id_compagnie) references Compagnie(id_iata_code),
+                                    foreign key(id_type_avion) references TypeAvion(id))"""
+    executer_requete(cur, req)
+    req = """create table Avion (id_reg text primary key,
+                                 id_compagnie text,
+                                 id_config integer,
                                  date_construction date,
                                  date_derniere_revision date,
                                  etat integer,
-                                 id_compagnie text,
                                  id_aeroport text,
-                                 id_type_avion integer,
                                  position text,
                                  foreign key(etat) references EnumAvion(id),
                                  foreign key(id_compagnie) references Compagnie(id_iata_code),
-                                 foreign key(id_aeroport) references Aeroport(id_iata_code),
-                                 foreign key(id_type_avion) references TypeAvion(id))"""
+                                 foreign key(id_config, id_compagnie) references ConfigAvion(id, id_compagnie),
+                                 foreign key(id_aeroport) references Aeroport(id_iata_code))"""
     executer_requete(cur, req)
     req = """create table Route (id integer primary key AUTOINCREMENT NOT NULL,
                                  id_compagnie text,
@@ -145,6 +154,38 @@ def creer_bdd(db_name):
 
     # Insertion des éléments
 
+    # EnumAvion
+    colonnes = ('etat')
+    enumavion = (
+        ("en revision"),
+        ("en vol"),
+        ("a terre")
+    )
+    for t in enumavion:
+        r.insert_into(cur, 'EnumAvion', colonnes, t)
+    valider_modifs(conn)
+
+    # EnumOption
+    colonnes = ('option')
+    enumavion = (
+        ("vegetarien")
+    )
+    for t in enumavion:
+        r.insert_into(cur, 'EnumAvion', colonnes, t)
+    valider_modifs(conn)
+
+    # EnumStatutVol
+    colonnes = ('statut')
+    enumavion = (
+        ("a l'heure"),
+        ("retarde"),
+        ("embarquement"),
+        ("annule")
+    )
+    for t in enumavion:
+        r.insert_into(cur, 'EnumAvion', colonnes, t)
+    valider_modifs(conn)
+
     # Aeroport
     colonnes = ('id_iata_code','type','nom','latitude_deg','longitude_deg','elevation_ft','continent','pays',
                 'municipalite','gps_code')
@@ -170,24 +211,46 @@ def creer_bdd(db_name):
     colonnes = ('id_iata_code','nom','icao_code','pays')
     compagnies = (
         ("NH","All Nippon Airways","ANA","Japan"),
-        ("CI","China Airlines","CAL","Taiwan")
+        ("BR", "EVA Air", "EVA", "Taiwan"),
+        ("CI","China Airlines","CAL","Taiwan"),
+        ("JL","Japan Airlines","JAL","Japan")
     )
     for t in compagnies:
         r.insert_into(cur, 'Compagnie', colonnes, t)
     valider_modifs(conn)
 
     # TypeAvion
-    colonnes = ('nom','nb_place_premiere','nb_place_business','nb_place_eco_plus','nb_place_eco','nb_total_place',
-                'fuel_cap_L','distance_franchissable_km','vitesse_mach','altitude_vol_m','distance_decollage_m')
+    colonnes = ('nom','iata_code','icao_code','fuel_cap_L','distance_franchissable_km','vitesse_mach',
+                'altitude_vol_m','distance_decollage_m')
     types = (
-        ("321",0,8,0,186,194,30000,5950,0.78,12000,2100),
-        ("CI", "NRT", "KHH", "LINESTRING(140.386001587 35.7647018433, 120.3499984741 22.5771007538)", 0)
+        ("A321","321","A321",30000,5950,0.78,12000,1700),
+        ("B767-300ER","763","B763",90770,11070,0.8,12000,2400)
     )
     for t in types:
-        r.insert_into(cur, 'Route', colonnes, t)
+        r.insert_into(cur, 'TypeAvion', colonnes, t)
+    valider_modifs(conn)
+
+    # ConfigAvion
+    colonnes = ('nom','id_compagnie','id_type_avion','nb_place_premiere','nb_place_business','nb_place_eco_plus',
+                'nb_place_eco','nb_total_place','disposition')
+    configs = (
+        ("321","BR","A321",0,8,0,176,184,""),
+        ("A44","JL","B767-300ER",0,24,0,175,199,"")
+    )
+    for t in configs:
+        r.insert_into(cur, 'ConfigAvion', colonnes, t)
     valider_modifs(conn)
 
     # Avion
+    colonnes = ('id_reg','id_compagnie','id_config','date_construction','date_derniere_revision','etat','id_aeroport',
+                'position')
+    configs = (
+        ("B-16213","BR","321","10/10/2014","27/10/2014",3,"KHH","POINT(120.3499984741211 22.57710075378418)"),
+        ("JA608J","JL","A44","23/02/2004","01/05/2014",3,"NRT","POINT(140.386001587 35.7647018433)")
+    )
+    for t in configs:
+        r.insert_into(cur, 'ConfigAvion', colonnes, t)
+    valider_modifs(conn)
 
     # Route
     colonnes = ('id_compagnie', 'id_aeroport_depart', 'id_aeroport_arrivee', 'geom', 'codeshare')
@@ -203,7 +266,7 @@ def creer_bdd(db_name):
     colonnes = ('id_compagnie','numero_vol','id_route','heure_depart','heure_arrivee','duree','periodicite',
                 'id_horaire_operateur','id_type_avion')
     horaires = (
-        ("NH", "NRT", "KHH", "LINESTRING(140.386001587 35.7647018433, 120.3499984741 22.5771007538)", 0),
+        ("NH",5831, "KHH", "LINESTRING(140.386001587 35.7647018433, 120.3499984741 22.5771007538)", 0),
         ("CI", "NRT", "KHH", "LINESTRING(140.386001587 35.7647018433, 120.3499984741 22.5771007538)", 0)
     )
     for t in horaires:

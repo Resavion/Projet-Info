ce411a7e4fa3ec5f18cfe0cc8e9431f997ed9326
Mathieu Margollé <mathieu.margolle@yandex.com>
2017-05-02 19:50:35 +0200

    Suite menus

--- __main__.py
+++ __main__.py
@@ -4,7 +4,7 @@ import ihm.console as ihm
 from bdd.config_bdd import (creer_bdd, inserer_jeu_test)
 from utilitaires.chargement import (charger_bd,
                                     update_bd)
-from utilitaires.menus import (ajouter_client)
+import utilitaires.menus as menus
 
 
 if __name__ == '__main__':
@@ -22,84 +22,10 @@ if __name__ == '__main__':
 
         # Si Client
         if choix == modes[0]:
-            # Choisir un client existant ou nouveau
-            comptes = [client for client in clients]
-            comptes.append('Nouveau client')
-            client = ihm.choisir(
-                comptes, "Choisissez un client sauvegardé ou "
-                         "créez un nouveau client")
-
-            # Si nouveau : demander infos et créer client
-            if client == 'Nouveau client':
-                client = ajouter_client(clients)
-                clients.append(client)
-
-            # Le client est chargé
-            ihm.afficher("Bonjour {} !".format(client.prenom))
-
-            # Demander action : Faire, Consulter, Modifier, Annuler
-            actions = ('Faire une réservation', 'Consulter ses réservations',
-                       'Modifier une réservation', 'Annuler une réservation',
-                       'Revenir au début')
-            action = ihm.choisir(actions, "Choisissez une action :")
-            if action == actions[0]:
-                client.faire_reservation()
-            elif action == actions[1]:
-                client.consulter_reservations()
-            elif action == actions[2]:
-                client.modifier_reservation()
-            elif action == actions[3]:
-                client.annuler_reservation()
-            else:
-                pass
-
+            menus.actions_client(clients)
         # Sinon : c'est un gestionnaire de compagnie
         elif choix == modes[1]:
-            # On demande de choisir une compagnie
-            actions = ('Recherche par continent',
-                       'Recherche par code IATA ou ICAO',
-                       'Revenir au début')
-            recherche = ihm.choisir(actions, "Choisissez un mode de recherche :")
-
-            # Si recherche par continents
-            if recherche == actions[0]:
-                continents = {'Amérique du Nord':'NA', 'Amérique du Sud':'SA',
-                              'Europe':'EU', 'Afrique':'AF', 'Asie':'AS',
-                              'Océanie':'OC'}
-                nom = ihm.choisir([*continents.keys()],
-                                  "Choisissez un continent :")
-                compagnies.sort(key=lambda s: len(s.routes),reverse=True)
-                compagnies_filtre = [x for x in compagnies
-                                     if x.code_continent == continents[nom]]
-                borne_bas = 0
-                pas = 2
-                compagnie = None
-                while True:
-                    borne_haut = min(len(compagnies_filtre), borne_bas+pas)
-                    liste = compagnies_filtre[borne_bas:borne_haut]
-                    if len(liste) == 0:
-                        ihm.afficher("Il n'y a pas de compagnie disponible !")
-                        break
-                    if borne_bas > 0:
-                        liste.append("Voir les compagnies précédentes")
-                    if borne_haut < len(compagnies_filtre):
-                        liste.append("Voir les compagnies suivantes")
-                    # Faire le choix
-                    compagnie = ihm.choisir(liste, "Choisissez une compagnie :")
-                    if compagnie == "Voir les compagnies suivantes":
-                        borne_bas = borne_haut
-                    elif compagnie == "Voir les compagnies précédentes":
-                        borne_haut = borne_bas
-                        borne_bas -= pas
-                    else:
-                        print(compagnie)
-                        break
-            # Si recherche par code
-            elif recherche == actions[1]:
-                test = True
-            # Sinon on revient au début
-            else:
-                pass
+            menus.actions_compagnie(compagnies)
         # Sinon on veut quitter le programme
         else:
             break
--- utilitaires/menus.py
+++ utilitaires/menus.py
@@ -4,6 +4,39 @@ import ihm.console as ihm
 from reservation.Client import Client
 
 
+def actions_client(clients):
+    # Choisir un client existant ou nouveau
+    comptes = [client for client in clients]
+    comptes.append('Nouveau client')
+    client = ihm.choisir(
+        comptes, "Choisissez un client sauvegardé ou "
+                 "créez un nouveau client")
+
+    # Si nouveau : demander infos et créer client
+    if client == 'Nouveau client':
+        client = ajouter_client(clients)
+        clients.append(client)
+
+    # Le client est chargé
+    ihm.afficher("Bonjour {} !".format(client.prenom))
+
+    # Demander action : Faire, Consulter, Modifier, Annuler
+    actions = ('Faire une réservation', 'Consulter ses réservations',
+               'Modifier une réservation', 'Annuler une réservation',
+               'Revenir au début')
+    action = ihm.choisir(actions, "Choisissez une action :")
+    if action == actions[0]:
+        client.faire_reservation()
+    elif action == actions[1]:
+        client.consulter_reservations()
+    elif action == actions[2]:
+        client.modifier_reservation()
+    elif action == actions[3]:
+        client.annuler_reservation()
+    else:
+        pass
+
+
 def ajouter_client(clients):
     """
     Crée un nouveau client
@@ -27,3 +60,93 @@ def ajouter_client(clients):
             break
     client = Client(new_id, nom, prenom, date_naiss)
     return client
+
+
+def actions_compagnie(compagnies):
+    # On demande de choisir une compagnie
+    actions = ('Recherche par continent',
+               'Recherche par code IATA ou ICAO',
+               'Revenir au début')
+    recherche = ihm.choisir(actions, "Choisissez un mode de recherche :")
+
+    # Si recherche par continents
+    if recherche == actions[0]:
+        continents = {'Amérique du Nord': 'NA', 'Amérique du Sud': 'SA',
+                      'Europe': 'EU', 'Afrique': 'AF', 'Asie': 'AS',
+                      'Océanie': 'OC'}
+        nom = ihm.choisir([*continents.keys()],
+                          "Choisissez un continent :")
+        compagnies.sort(key=lambda s: len(s.routes), reverse=True)
+        compagnies_filtre = [x for x in compagnies
+                             if x.code_continent == continents[nom]]
+        borne_bas = 0
+        pas = 2
+        compagnie = None
+        while True:
+            borne_haut = min(len(compagnies_filtre), borne_bas + pas)
+            liste = compagnies_filtre[borne_bas:borne_haut]
+            if len(liste) == 0:
+                ihm.afficher("Il n'y a pas de compagnie disponible !")
+                break
+            if borne_bas > 0:
+                liste.append("Voir les compagnies précédentes")
+            if borne_haut < len(compagnies_filtre):
+                liste.append("Voir les compagnies suivantes")
+            # Faire le choix
+            compagnie = ihm.choisir(liste, "Choisissez une compagnie :")
+            if compagnie == "Voir les compagnies suivantes":
+                borne_bas = borne_haut
+            elif compagnie == "Voir les compagnies précédentes":
+                borne_haut = borne_bas
+                borne_bas -= pas
+            else:
+                print(compagnie)
+                break
+    # Si recherche par code
+    elif recherche == actions[1]:
+        test = True
+    # Sinon on revient au début
+    else:
+        pass
+
+
+def choisir_par_continent(compagnies):
+    # On choisit un continent
+    continents = {'Amérique du Nord': 'NA', 'Amérique du Sud': 'SA',
+                  'Europe': 'EU', 'Afrique': 'AF', 'Asie': 'AS',
+                  'Océanie': 'OC'}
+    nom = ihm.choisir([*continents.keys()],
+                      "Choisissez un continent :")
+
+    # On ne garde que les compagnies du continent
+    compagnies_filtre = [x for x in compagnies
+                         if x.code_continent == continents[nom]]
+    if len(compagnies_filtre) == 0:
+        ihm.afficher("Il n'y a pas de compagnie disponible !")
+        return None
+
+    # On filtre les compagnies par nombre de routes
+    compagnies_filtre.sort(key=lambda s: len(s.routes), reverse=True)
+
+    borne_bas = 0
+    pas = 2
+    compagnie = None
+    while True:
+        borne_haut = min(len(compagnies_filtre), borne_bas + pas)
+        # On affiche seulement quelques compagnies à la fois
+        liste = compagnies_filtre[borne_bas:borne_haut]
+        if borne_bas > 0:
+            liste.append("Voir les compagnies précédentes")
+        if borne_haut < len(compagnies_filtre):
+            liste.append("Voir les compagnies suivantes")
+        # Faire le choix
+        compagnie = ihm.choisir(liste, "Choisissez une compagnie :")
+        if compagnie == "Voir les compagnies suivantes":
+            borne_bas = borne_haut
+        elif compagnie == "Voir les compagnies précédentes":
+            borne_haut = borne_bas
+            borne_bas -= pas
+        else:
+            print(compagnie)
+            break
+    return compagnie

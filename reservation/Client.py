e9dcb727d4f09653ab2fc4cbbe8b469bccd666df
Mathieu Margollé <mathieu.margolle@yandex.com>
2017-05-11 00:00:25 +0200

    derniere modif

--- reservation/Client.py
+++ reservation/Client.py
@@ -151,11 +151,11 @@ class Client(object):
                 if Segment.liste_ids:
                     id_segment = Segment.liste_ids[-1]
                 id_segment += 1
-                seg = Segment(id_segment, billet, vol, '', '', place, '', classe)
+                seg = Segment(id_segment, billet, vol, '', place, '', classe)
                 billet.segments.append(seg)
 
             new_resa.billets.append(billet)
-            
+
         self.reservations.append(new_resa)
         return
 
